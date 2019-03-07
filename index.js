4a92bc68d1060f6239ced36acaea3d75bb3dd293
Lennox Stevenson <lennox.stevenson@mail.utoronto.ca>
2019-03-06 21:12:23 -0500

    feat: only list employees that have departed since last run

--- index.js
+++ index.js
@@ -1,10 +1,26 @@
 const axios = require('axios');
 const moment = require('moment');
-const { orderBy } = require('lodash');
+const { orderBy, differenceWith, isEqual } = require('lodash');
+const fs = require('fs');
+const os = require('os');
 
 const { TOKEN } = process.env;
 
-const main = async () => {
+const STATE_FILENAME = 'state.json';
+
+const getState = () => {
+    try {
+        return JSON.parse(fs.readFileSync(STATE_FILENAME));
+    } catch (e) {
+        return [];
+    }
+};
+
+const setState = state => {
+    fs.writeFileSync(STATE_FILENAME, JSON.stringify(state, null, 4) + os.EOL);
+};
+
+const fetchAllDepartedEmployees = async () => {
     const response = await axios.get('https://slack.com/api/users.list', {
         params: { token: TOKEN },
     });
@@ -17,14 +33,28 @@ const main = async () => {
 
     const sortedDeletedMembers = orderBy(deletedMembers, ['updated'], ['asc']);
 
-    console.log(`Total Number of Employees: ${sortedDeletedMembers.length}`);
-
-    const output = sortedDeletedMembers.map(({ name, updated }) => ({
+    return sortedDeletedMembers.map(({ name, updated }) => ({
         name,
         updated: moment.unix(updated).format('dddd, MMMM Do YYYY, h:mm:ss a'),
     }));
+};
 
-    console.log(output);
+const listRecentlyDepartedEmployees = (
+    departedEmployees,
+    previouslyDepartedEmployees
+) => differenceWith(departedEmployees, previouslyDepartedEmployees, isEqual);
+
+const main = async () => {
+    const pastState = getState();
+
+    const departedEmployees = await fetchAllDepartedEmployees();
+
+    console.log(
+        'Recently Departed Employees: ',
+        listRecentlyDepartedEmployees(departedEmployees, pastState)
+    );
+
+    setState(departedEmployees);
 };
 
 main().catch(e => {
